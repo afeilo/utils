110142856b1a0b312690e9c3c1688d5777f9a2f5
woshizhu <935437130@qq.com>
2016-04-07 09:06:06 +0800

    SharePreferences工具类

---
+++ SPUtil.java
@@ -0,0 +1,176 @@
+package com.xiefei.musicplayer.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * Created by xiefei on 2016/3/7.
+ */
+public class SPUtil {
+    public SPUtil(Context context,String fileName){
+        this(context,fileName,Context.MODE_PRIVATE);
+    };
+    public SPUtil(Context context,String fileName,int mode){
+        sharedPreferences = context.getSharedPreferences(fileName,mode);
+    }
+    private SharedPreferences sharedPreferences;
+    /**
+     * 保存数据的方法，我们需要拿到保存数据的具体类型，然后根据类型调用不同的保存方法
+     *
+     * @param key
+     * @param object
+     */
+    public void put( String key, Object object)
+    {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        if (object instanceof String)
+        {
+            editor.putString(key, (String) object);
+        } else if (object instanceof Integer)
+        {
+            editor.putInt(key, (Integer) object);
+        } else if (object instanceof Boolean)
+        {
+            editor.putBoolean(key, (Boolean) object);
+        } else if (object instanceof Float)
+        {
+            editor.putFloat(key, (Float) object);
+        } else if (object instanceof Long)
+        {
+            editor.putLong(key, (Long) object);
+        } else
+        {
+            editor.putString(key, object.toString());
+        }
+
+        SharedPreferencesCompat.apply(editor);
+    }
+
+    /**
+     * 得到保存数据的方法，我们根据默认值得到保存的数据的具体类型，然后调用相对于的方法获取值
+     *
+     * @param key
+     * @param defaultObject
+     * @return
+     */
+    public Object get(String key, Object defaultObject)
+    {
+
+        if (defaultObject instanceof String)
+        {
+            return sharedPreferences.getString(key, (String) defaultObject);
+        } else if (defaultObject instanceof Integer)
+        {
+            return sharedPreferences.getInt(key, (Integer) defaultObject);
+        } else if (defaultObject instanceof Boolean)
+        {
+            return sharedPreferences.getBoolean(key, (Boolean) defaultObject);
+        } else if (defaultObject instanceof Float)
+        {
+            return sharedPreferences.getFloat(key, (Float) defaultObject);
+        } else if (defaultObject instanceof Long)
+        {
+            return sharedPreferences.getLong(key, (Long) defaultObject);
+        }
+        return null;
+    }
+
+    /**
+     * 移除某个key值已经对应的值
+     * @param key
+     */
+    public void remove(String key)
+    {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove(key);
+        SharedPreferencesCompat.apply(editor);
+    }
+
+    /**
+     * 清除所有数据
+     */
+    public void clear()
+    {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        SharedPreferencesCompat.apply(editor);
+    }
+
+    /**
+     * 查询某个key是否已经存在
+     * @param key
+     * @return
+     */
+    public boolean contains(String key)
+    {
+        return sharedPreferences.contains(key);
+    }
+
+    /**
+     * 返回所有的键值对
+     * @return
+     */
+    public Map<String, ?> getAll(Context context)
+    {
+        return sharedPreferences.getAll();
+    }
+
+    /**
+     * 创建一个解决SharedPreferencesCompat.apply方法的一个兼容类
+     *
+     * @author zhy
+     *
+     */
+    private static class SharedPreferencesCompat
+    {
+        private static final Method sApplyMethod = findApplyMethod();
+
+        /**
+         * 反射查找apply的方法
+         *
+         * @return
+         */
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        private static Method findApplyMethod()
+        {
+            try
+            {
+                Class clz = SharedPreferences.Editor.class;
+                return clz.getMethod("apply");
+            } catch (NoSuchMethodException e)
+            {
+            }
+
+            return null;
+        }
+
+        /**
+         * 如果找到则使用apply执行，否则使用commit
+         *
+         * @param editor
+         */
+        public static void apply(SharedPreferences.Editor editor)
+        {
+            try
+            {
+                if (sApplyMethod != null)
+                {
+                    sApplyMethod.invoke(editor);
+                    return;
+                }
+            } catch (IllegalArgumentException e)
+            {
+            } catch (IllegalAccessException e)
+            {
+            } catch (InvocationTargetException e)
+            {
+            }
+            editor.commit();
+        }
+    }
+}
